7ed553ba863b9f07a6fafe4ca3a6c04f58c54171
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-23 17:38:01 -0300

    obtendo dados sobre perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -8,18 +8,63 @@ import '../styles/room.scss';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
+  },
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}>
+
 type RoomParams = {
   id: string;
+}
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  },
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
 }
 
 export function Room() {
   const { id } = useParams<RoomParams>();
   const { user } = useAuth();
   const [newQuestion, setNewQuestion] = useState('');
+  const [title, setTitle] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${id}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          author: value.author,
+          content: value.content,
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered
+        }
+      })
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+      
+    });
+  }, [id]);
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
-    if ( newQuestion.trim() === '') return;
+    if (newQuestion.trim() === '') return;
 
     if (!user) throw new Error("You must be logged in");
 
@@ -49,30 +94,34 @@ export function Room() {
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala {title}</h1>
+          { questions.length > 0 && (
+            <span>{questions.length} pergunta(s)</span>
+          )}
         </div>
 
         <form onSubmit={handleSendQuestion}>
-          <textarea 
+          <textarea
             placeholder="O que você quer perguntar?"
             onChange={event => setNewQuestion(event.target.value)}
             value={newQuestion}
           />
 
           <div className="form-footer">
-            { user ? (
+            {user ? (
               <div className="user-info">
                 <img src={user.avatar} alt={user.name} />
                 <span>{user.name}</span>
               </div>
             ) : (
-               <span>Para enviar uma pergunta, <button>Faça seu login</button>.</span>
+              <span>Para enviar uma pergunta, <button>Faça seu login</button>.</span>
             )}
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
+
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
-}
+};
