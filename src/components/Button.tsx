be6f99207fad4ff16d238fac247723b228943425
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-06-24 21:15:48 -0300

    estrutura da pagina de admin da sala

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 import { ButtonHTMLAttributes } from 'react';
 import '../styles/button.scss'
 
-type ButaoProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButaoProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
 
-export function Button(props: ButaoProps) {
+export function Button({ isOutlined = false, ...props }: ButaoProps) {
   return (
-    <button className="button" {...props} />
+    <button 
+      className={`button ${isOutlined ? 'outlined': ''}`}
+      {...props}
+    />
   );
 }
